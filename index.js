00a8c1b03758e3ac3804a4fe1097a4fbee3a64d4
vlorian <vlorian.dervishi@gmail.com>
2023-08-08 20:34:21 -0400

    passed all tests

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Divides a number by 100.
+ * @param {number} num - The number to divide.
+ * @returns The result of dividing the number by 100.
+ */
+const divide = function (num) {
+  return num / 100;
+};
+
+/**
+ * Calculates the square of a given number.
+ * @param {number} num - The number to calculate the square of.
+ * @returns The square of the given number.
+ */
+const square = (num) => {
+  return num * num;
+};
+/**
+ * Adds two numbers together.
+ * @param {number} prev - The first number to add.
+ * @param {number} curr - The second number to add.
+ * @returns The sum of the two numbers.
+ */
+const add = (prev, curr) => {
+  return prev + curr;
+};
